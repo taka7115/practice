00296291dc5066af5718330723ff777cb5fe7a29
3kaku-nishina <takahiro.nishina@3kaku.co.jp>
2020-07-17 15:58:26 +0900

    transfer to new pc

--- web/web/assets/js/coding/c-page.js
+++ web/web/assets/js/coding/c-page.js
@@ -29001,17 +29001,28 @@ function func() {
 "use strict";
 
 
+__webpack_require__(/*! core-js/modules/es.array.for-each */ "./node_modules/core-js/modules/es.array.for-each.js");
+
 __webpack_require__(/*! core-js/modules/es.object.define-property */ "./node_modules/core-js/modules/es.object.define-property.js");
+
+__webpack_require__(/*! core-js/modules/web.dom-collections.for-each */ "./node_modules/core-js/modules/web.dom-collections.for-each.js");
+
+__webpack_require__(/*! core-js/modules/es.array.for-each */ "./node_modules/core-js/modules/es.array.for-each.js");
+
+__webpack_require__(/*! core-js/modules/web.dom-collections.for-each */ "./node_modules/core-js/modules/web.dom-collections.for-each.js");
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
+
+var _jquery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+
 var _default = {
-  id: 3,
+  id: 8,
   ttl: "<span></span><br class='u-sp'>",
-  txt: "3作品目の情報が入ります。",
-  alt: "3作品目の情報が入ります。",
+  txt: "8作品目の情報が入ります。",
+  alt: "8作品目の情報が入ります。",
   p1: "",
   p1_color: "js-yellow",
   p1_list1: "<span data='dot'>&#9642;</span>",
@@ -29032,7 +29043,117 @@ var _default = {
 
 exports["default"] = _default;
 
-function func() {}
+function func() {
+  //親要素とcanvas要素を取得
+  var canvasParent = document.getElementById("canvasParent");
+  var canvas = document.getElementById("canvas"); // Canvas利用不可の環境では実行しないようにif文で囲む
+
+  if (canvas.getContext) {
+    // ------------------------------------------------------------------
+    // ユーティリティー関数
+    // // 円の描画座標出力
+    var randomIntFromRange = function randomIntFromRange(area, radius) {
+      return Math.floor(Math.random() * (area - radius + 1) + radius);
+    }; // ------------------------------------------------------------------
+    //関数オブジェクトを設定
+
+
+    var Particle = function Particle(x, y, radius) {
+      var _this = this;
+
+      this.x = x;
+      this.y = y;
+      this.velocity = 0.05;
+      this.radius = radius; // this.velocity = {
+      //   x: (Math.random() - 0.5) * val.velocityRange,
+      //   y: (Math.random() - 0.5) * val.velocityRange
+      // }
+
+      this.radians = Math.random() * Math.PI * 2; // colorArrayに格納された色をランダムに割り振る
+
+      this.color = val.colorArray[Math.floor(Math.random() * val.colorArray.length)];
+      this.distanceFromCenter = randomIntFromRange(50, 120); // 円を描画する処理
+
+      this.draw = function () {
+        c.beginPath();
+        c.strokeStyle = _this.color;
+        c.lineWidth = _this.radius;
+        c.moveTo();
+        c.lineTo();
+        c.stroke();
+        c.closePath();
+      };
+
+      this.lastMouse = {
+        x: x,
+        y: y
+      }; // 座標をズラしながら円を描画していく処理
+
+      this.update = function () {
+        // move points over timers
+        _this.radians += _this.velocity; // 円形移動
+
+        _this.x = x + Math.cos(_this.radians) * _this.distanceFromCenter;
+        _this.y = y + Math.sin(_this.radians) * _this.distanceFromCenter; // // 範囲の端にきたら折り返す処理
+        // if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+        //   this.velocity.x = -this.velocity.x;
+        // } else if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+        //   this.velocity.y = -this.velocity.y;
+        // }
+        // 座標の値を変えていく
+
+        _this.x += _this.velocity.x;
+        _this.y += _this.velocity.y; // 円を描画する
+
+        _this.draw();
+      };
+    }; // ------------------------------------------------------------------
+    // 円それぞれの値を配列に格納する関数
+
+
+    var init = function init() {
+      particles = [];
+
+      for (var i = 0; i < 50; i++) {
+        particles.push(new Particle(canvas.width / 2, canvas.height / 2, 5, "blue"));
+      }
+    }; // ------------------------------------------------------------------
+    // 円を描画&アニメーションさせる関数
+
+
+    var animate = function animate() {
+      // animate関数をループさせる関数
+      requestAnimationFrame(animate);
+      c.fillStyle = 'rgba(255,255,255,0.05)'; // 指定した範囲の描画内容をリセットする
+
+      c.clearRect(0, 0, canvas.width, canvas.height);
+      particles.forEach(function (particle) {
+        particle.update();
+      });
+    }; // ------------------------------------------------------------------
+    // init関数の実行
+    // canvasの幅と高さを親要素のサイズに合わせる
+
+
+    canvas.width = canvasParent.clientWidth;
+    canvas.height = canvasParent.clientHeight; // Canvasに描画機能を付与
+
+    var c = canvas.getContext('2d'); // ------------------------------------------------------------------
+    // 円の描画&アニメーションに必要な値をオブジェクト化しておく
+
+    var val = {
+      quantity: 100,
+      radius: 20,
+      colorArray: ["#F27EBE", "#3DF2BF", "#05AFF2", "#F2E085", "#F24822"],
+      velocityRange: 5,
+      lineWidth: 2
+    };
+    var particles;
+    init(); // animate関数の実行
+
+    animate();
+  }
+}
 
 /***/ }),
 
