d37936d3335497a940ccccdc5d6682400980f76f
taka7115 <taka7115web@gmail.com>
2020-10-05 09:14:22 +0900

    navigation

--- web/src/assets/js/coding/data/work12.js
+++ web/src/assets/js/coding/data/work12.js
@@ -26,85 +26,97 @@ export default {
 
 
 function func() {
-  alert("ok");
+
+  // sectionの配列
+  var secArray = [];
+
+  // ナビゲーションのリンクを取得
+  var secList = document.querySelectorAll(".js-target");
+
+  var navList = document.querySelectorAll(".js-list");
 
   // 特徴ページ。スクロール量によってコンテンツナビゲーションのアンダーラインの位置を変える
   var addLine = () => {
 
-    // ナビゲーションのリンクを指定
-    var navList = document.querySelectorAll(".nav-list");
 
-    // 各コンテンツのページ上部からの開始位置と終了位置を配列に格納しておく
-    var sectionArr = [];
 
-    for (var i = 0; i < navList.length; i++) {
+    for (var i = 0; i < secList.length; i++) {
       // コンテンツのIDを取得
-      var targetContents = navList[i].find("a").getAttribute("href");
-      // ページ内リンクでないナビゲーションが含まれている場合は除外する
-      if (targetContents.charAt(0) == "#") {
+      var target = secList[i];
+      console.log(target);
 
-        // ページ上部からコンテンツの開始位置までの距離を取得
-        var targetContentsTop = targetContents.offsetTop;
-        // ページ上部からコンテンツの終了位置までの距離を取得
-        var targetContentsBottom = targetContentsTop + targetContents.clientHeight - 1;
-        // 配列に格納
-        sectionArr[i] = [targetContentsTop, targetContentsBottom]
-      }
+
+      // ページ上部からコンテンツの開始位置までの距離を取得
+      var secTop = target.offsetTop;
+      console.log(`コンテンツ開始位置までの距離：${secTop}`)
+      // ページ上部からコンテンツの終了位置までの距離を取得
+      var secBottom = secTop + target.clientHeight - 1;
+      console.log(`コンテンツ終了位置までの距離：${secTop}`)
+      // 配列に格納
+      secArray[i] = [secTop, secBottom]
     };
 
-    // 現在地をチェックする
-    var currentCheck = () => {
-      // 現在のスクロール位置を取得
-      var wS = document.documentElement.scrollTop || document.body.scrollTop;
-      // 画面の高さを取得
-      var wH = window.clientHeight;
-      // 現在のスクロール位置を画面中央に指定
-      var windowScrolltop = wS + (wH / 2);
+    console.log(`配列：${secArray}`)
 
-      for (var i = 0; i < sectionArr.length; i++) {
-        // 現在のスクロール位置が、配列に格納した開始位置と終了位置の間にあるものを調べる
-        if (sectionArr[i][0] <= windowScrolltop) {
-          //  && sectionArr[i][1] >= windowScrolltop
-          // 開始位置と終了位置の間にある場合、ナビゲーションにclass="current"をつける
-          navList.classList.remove('underline');
-          navList[i].classList.add('underline');
-          i == sectionArr.length;
-        }
-      };
-    }
+  } //addLine()
 
-    // スクロール時に、現在地をチェックする
-    document.querySelector(".wrapper").addEventListener("scroll", () => {
-      console.log("ok");
-      currentCheck();
-    });
 
-  } //	fNavUnderLine()
+
+  // 現在地をチェックする
+  var currentCheck = () => {
+
+    // スクロールする要素の取得
+    var base = document.querySelector(".wrapper");
+
+    // 現在のスクロール位置を取得
+    var scrollVolume = base.scrollTop;
+
+
+
+    for (var i = 0; i < secArray.length; i++) {
+      // 現在のスクロール位置が、配列に格納した開始位置と終了位置の間にあるものを調べる
+      if (secArray[i][0] <= scrollVolume && secArray[i][1] >= scrollVolume) {
+        //  && secArray[i][1] >= scrollVolume
+        // 開始位置と終了位置の間にある場合、ナビゲーションにclass="current"をつける
+        i == secArray.length;
+        navList[i].classList.add('underline');
+      } else {
+        navList[i].classList.remove('underline');
+      }
+    };
+  } //currentCheck
 
   window.addEventListener("load", () => {
     addLine();
   })
 
+  // スクロール時に、現在地をチェックする
+  document.querySelector(".wrapper").addEventListener("scroll", () => {
+    currentCheck();
+  });
 
-  const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
-  for (let i = 0; i < smoothScrollTrigger.length; i++) {
-    smoothScrollTrigger[i].addEventListener('click', (e) => {
-      e.preventDefault();
-      let href = smoothScrollTrigger[i].getAttribute('href');
-      let targetElement = document.getElementById(href.replace('#', ''));
-      let area = document.querySelector(".cont");
-      const rect = targetElement.getBoundingClientRect().top;
-      const offset = window.pageYOffset;
-      const gap = 60;
-      const target = rect + offset - gap;
-      area.scrollTo({
-        top: target,
-        behavior: 'smooth',
-      });
-    });
-  }
 
-  smoothScrollTrigger();
+
+
+
+  // const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+  // for (let i = 0; i < smoothScrollTrigger.length; i++) {
+  //   smoothScrollTrigger[i].addEventListener('click', (e) => {
+  //     e.preventDefault();
+  //     let href = smoothScrollTrigger[i].getAttribute('href');
+  //     let targetElement = document.getElementById(href.replace('#', ''));
+  //     let area = document.querySelector(".cont");
+  //     const rect = targetElement.getBoundingClientRect().top;
+  //     const offset = window.pageYOffset;
+  //     const gap = 60;
+  //     const target = rect + offset - gap;
+  //     area.scrollTo({
+  //       top: target,
+  //       behavior: 'smooth',
+  //     });
+  //   });
+  // }
+
 
 
 
